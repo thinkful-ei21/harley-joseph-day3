f4e6f9013e57ffb8f59b842655474940977f888e
Joseph Kim <gyuhankim@gmail.com>
2018-05-09 15:46:30 -0400

    add Factory Functions with LOTR

--- drills2.js
+++ drills2.js
@@ -60,7 +60,7 @@ function wordToLetter(word) {
     d:4
   };
   if(cipher[firstLetter]){
-    return word.charAt(cipher[firstLetter])
+    return word.charAt(cipher[firstLetter]);
   } else {
     return ' ';
   }
@@ -80,3 +80,32 @@ function decodeWords(stringOfWords){
 }
 
 console.log (decodeWords('craft block argon meter bells brown croon droop'));
+
+function createCharacter(name, nickname, race, origin, attack, defense){
+  return {
+    name: name,
+    nickname: nickname,
+    race: race,
+    origin: origin,
+    attack: attack,
+    defense: defense,
+    describe: function(){
+      console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+    },
+    evaluateFight: function(character){
+      let x = (this.attack - character.defense) > 0 ? (this.attack - character.defense) : 0;
+      let y = (character.attack - this.defense) > 0 ? (character.attack - this.defense) : 0;
+      console.log(`Your opponent takes ${x} damage and you receive ${y} damage`);
+    }
+  };
+}
+
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 7),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5)
+];
+
+characters.push(createCharacter('Joseph', 'joe', 'bro', 'Atlanta', 10, 10));
