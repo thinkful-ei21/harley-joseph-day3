54f9f40430578da8aa411da19967cd038a3a488b
Joseph Kim <gyuhankim@gmail.com>
2018-05-09 15:02:43 -0400

    add properties that arent there

--- drills2.js
+++ drills2.js
@@ -30,12 +30,23 @@ console.log(hobbitsMeals.meals[3]);
 
 const starTrek = [
   {name:'Picard',jobTitle:'Captain'},
-  {name:'Riker',jobTitle:'Number One'},
-  {name:'LaForge',jobTitle:'Chief Engineer'}
+  {name:'Riker',jobTitle:'Number One', boss:'Picard'},
+  {name:'LaForge',jobTitle:'Chief Engineer', boss:'Picard'}
 ];
 
-for(let i in starTrek){
-  for (let officer in starTrek[i]){
-    console.log(`${officer}: ${starTrek[i][officer]}`);
+// for(let i in starTrek){
+//   for (let officer in starTrek[i]){
+//     console.log(`${starTrek[i][officer]} `);
+//   }
+// }
+
+function starTrekMessages(arr) {
+  for (let i = 0; i < starTrek.length; i++){
+    if (starTrek[i].boss){
+      console.log(`${starTrek[i].jobTitle} ${starTrek[i].name} reports to ${starTrek[i].boss}`);
+    } else {
+      console.log(`${starTrek[i].jobTitle} ${starTrek[i].name} reports to no one.`);
+    }
   }
 }
+starTrekMessages(starTrek);
